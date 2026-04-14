2283f05aa5b89806d9869205ac3d86572dbaf5d9
Omnia Ahmed Marzouk <omniaahmedmarzouk@gmail.com>
2026-04-14 04:07:54 +0200

    Rename model file from model1.pkl to model.pkl

--- Online_retail_UCI/app.py
+++ Online_retail_UCI/app.py
@@ -5,7 +5,7 @@ import os
 
 # Get the directory where app.py is located
 base_path = os.path.dirname(__file__)
-model_path = os.path.join(base_path, 'model1.pkl')
+model_path = os.path.join(base_path, 'model.pkl')
 
 # Load the model using the absolute path
 model = joblib.load(model_path)
