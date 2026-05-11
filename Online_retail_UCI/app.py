aa92b6e7dc7f1468cbc309b987abca5cb33409f5
omnia <omniaahmedmarzouk@gmail.com>
2026-05-12 01:57:04 +0300

    Add Online Retail ML PipeLine project

--- Online_retail_UCI/app.py
+++ Online_retail_UCI/app.py
@@ -3,11 +3,8 @@ import joblib
 import numpy as np
 import os
 
-# Get the directory where app.py is located
 base_path = os.path.dirname(__file__)
 model_path = os.path.join(base_path, 'model.pkl')
-
-# Load the model using the absolute path
 model = joblib.load(model_path)
 
 st.title('Customer Value Predictor')
@@ -16,11 +13,10 @@ recency = st.number_input('Recency (days)', min_value=0)
 frequency = st.number_input('Frequency', min_value=0)
 r_score = st.slider('R Score', 1, 5)
 f_score = st.slider('F Score', 1, 5)
-m_score = st.slider('M Score', 1, 5)
-rfm_total = r_score + f_score + m_score
+rfm_total = r_score + f_score
 
 if st.button('Predict'):
-    X = np.array([[recency, frequency, r_score, f_score, m_score, rfm_total]])
+    X = np.array([[recency, frequency, r_score, f_score, rfm_total]])
     prediction = model.predict(X)
     
     if prediction[0] == 1:
